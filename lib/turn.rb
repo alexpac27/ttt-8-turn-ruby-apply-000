0d304cac7b3afa700e454745d2a8263d682909b3
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-09 21:50:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def turn(board)
 
   index = input_to_index(user_input)
 
-  if valid_move?(board,index)
+  if valid_move?(board,index) == true
     move(board,index)
     display_board(board)
   else
